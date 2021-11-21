304a7cb25d2ae9dbc86ec720d9a4684bd1bc2c7c
Giovanni Sabatelli <gsabatelli86@gmail.com>
2021-11-21 15:05:11 -0500

    Format File + More Features
    * Win, Loss, and Error Outcomes
    * WordBank

--- mainlog.py
+++ mainlog.py
@@ -1,45 +1,66 @@
-#Hangman Attempt
+# HangmanPython
 
-#Imports
-# sourcery skip: for-index-underscore
+# Imports
 import hangstages as hs
 import time
+import random
 
-#Initialization
-conf = hs.config(True); currRes = round(conf["desiredWidth"]/10)*10
+# Initialization
+with open("D:\\OldFiles\\FullTop\\LeetcodeAttempt\\hangman\\wordBank.txt", "r+") as words:
+    wordBank = words.read().split(", ")
+
+conf = hs.config(True)
+currRes = round(conf["desiredWidth"]/10)*10
 startText = "Welcome to my Python 3.9 Implementation of Hangman"
-customSpace = " " * ((currRes-+len(startText))//2)
-word = "UNITED ARAB EMIRATES"; charDict = hs.wordDict(word)
+customSpace = " " * ((currRes-len(startText))//2)
+word = random.choice(wordBank) + " " + \
+    random.choice(wordBank) + " " + random.choice(wordBank)
+charDict = hs.wordDict(word)
 guesses = 0
+status = "playing"
 
-#Console Start
-print("-"*currRes); print(customSpace+startText)
+# Console Start
+print("-"*currRes)
+print(customSpace+startText)
 
-#Resolution Change Question
+
+# Resolution Change Question
 resList = hs.spriteRes(currRes)
 
-#Initial Guess
+# Initial Guess
 hs.displayCurrStage(resList, True)
 hiddenPhrase = hs.guessHandler(word, None, None, True)
 
-while "_" in hiddenPhrase:
+while status == "playing":
     print("\n" + hiddenPhrase + "\n")
     guess = input("What letter do you want to guess?: ")
+
     newPhrase = hs.guessHandler(hiddenPhrase, charDict, guess)
 
-    if newPhrase == False:
-        print("\nSorry that letter isn't in the phrase!")
+    if newPhrase == "False":
+        print("\nSorry that letter is not in the phrase!")
         guesses += 1
         time.sleep(1)
         hs.displayCurrStage(resList)
+        if len(resList) == 1:
+            status = "loss"
+    elif "Error" in newPhrase:
+        status = "error"
     else:
         hiddenPhrase = newPhrase
-        print(f"\nGood Guess, that letter was in the phrase {len(charDict[guess.lower()])} times!")
+        print(
+            f"\nGood Guess, that letter was in the phrase {len(charDict[guess.lower()])} times!")
         guesses += 1
         time.sleep(1)
         hs.displayCurrStage(resList, True)
         if "_" not in hiddenPhrase:
             print("\n" + hiddenPhrase + "\n")
-        
-print(f"You guessed the secret word in only {guesses} guesses!\n")
+            status = "win"
 
+match status:
+    case "win":
+        print(f"You guessed the secret word in only {guesses} guesses!\n")
+    case "loss":
+        print(f"You Ran of Lives! The secret word was {word}!")
+    case "error":
+        print(newPhrase)
